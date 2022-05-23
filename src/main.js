9705c8833ec282c06e00181344994e1d23308526
Jehiko Mosquera <jeramos04@hotmail.com>
2022-05-22 19:17:23 -0500

    Vista Categories creada

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+//import router from '/.router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
---
+++ src/views/Categories.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container" id="Categorie">
+    <h1>Categories List</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NAME</th>
+                <th scope="col">DESCRIPTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="categorie in categories">
+                <th scope="row"> {{ categorie.id }}</th>
+                <td>{{ categorie.name }}</td>
+                <td>{{ categorie.description }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default{
+
+    name: 'Categorie',
+    data(){
+        return{
+            categories: []
+        }
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/categories').then(response => {this.categories = response.data.categories;
+        });
+    },
+
+}
+
+</script>
